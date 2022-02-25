1e31dc417d830fba9267d359a2eb74dbd11dc36a
Adam Walford <>
2022-02-25 19:27:14 +0000

    remove ref to admin

--- deployment/terraform/auth.tf
+++ deployment/terraform/auth.tf
@@ -3,7 +3,8 @@ resource "aws_api_gateway_authorizer" "propertyservice_api_authorizer" {
   name          = "propertyservice-authorizer"
   rest_api_id   = aws_api_gateway_rest_api.propertyservice_api.id
   type          = "COGNITO_USER_POOLS"
-  provider_arns = concat(tolist(data.aws_cognito_user_pools.propertyservice_userpool.arns), tolist(data.aws_cognito_user_pools.propertyservice_admin_userpool.arns))
+  provider_arns = tolist(data.aws_cognito_user_pools.propertyservice_userpool.arns)
+  # provider_arns = concat(tolist(data.aws_cognito_user_pools.propertyservice_userpool.arns), tolist(data.aws_cognito_user_pools.propertyservice_admin_userpool.arns))
 }
 
 # resource "aws_api_gateway_authorizer" "propertyservice_api_admin_authorizer" {
